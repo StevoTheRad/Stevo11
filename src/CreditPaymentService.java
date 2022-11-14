cd72b491fdeefd11a4748d6fa64a0c4eb9ae9121
Sergey Petrenko <Stevo98@mail.ru>
2022-11-14 14:05:30 +1000

    Corrected mistake

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,7 @@
 public class CreditPaymentService {
 
-    public int calculate(int sum, int monthAmount) {
-        return (int) (sum * (0.008325 + (0.008325 / (Math.pow(1.008325, monthAmount) - 1))));
+    public double calculate(int sum, int monthAmount) {
+        double percent = 9.99 / (100 * 12);// 1/100 доля процентной ставки в месяц согласно формуле (не зависит от срока займа)
+        return (sum * (percent + (percent / (Math.pow(1 + percent, monthAmount) - 1))));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int creditSum = 1000_000;// Сумма кредита
-        int loanTerm = 3;// Срок займа в годах
-        int payment = service.calculate(creditSum, 12 * loanTerm); // Ежемесячный платеж
+        int loanTerm = 2;// Срок займа в годах
+        int payment = (int) service.calculate(creditSum, loanTerm * 12); // Ежемесячный платеж
         System.out.println("Ежемесячный платеж составляет - " + payment + " руб.");
     }
 }
